bb6204daabed798e90b0ff07c880f1e4e5c91b5c
Dobrota <dobrota.art@gmail.com>
2024-07-03 17:27:36 +0300

    update

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import {
-<<<<<<< Updated upstream
   createContact,
   getAllContacts,
   getContactById,
@@ -9,40 +8,15 @@ import {
 } from '../controllers/contacts.js';
 import authenticate from '../middleware/authenticate.js';
 import { upload } from '../middleware/upload.js';
-=======
-  getAllContactsController,
-  getContactByIdController,
-  createContactController,
-  updateContactController,
-  deleteContactController,
-} from '../controllers/contacts.js';
-import authenticate from '../middleware/authenticate.js'; // Перевірте цей рядок
-import { validateBody } from '../middleware/validateBody.js';
-import Joi from 'joi';
->>>>>>> Stashed changes
 
 const router = express.Router();
 
 router.use(authenticate);
 
-<<<<<<< Updated upstream
 router.post('/', upload.single('photo'), createContact);
 router.get('/', getAllContacts);
 router.get('/:contactId', getContactById);
 router.patch('/:contactId', upload.single('photo'), updateContact);
 router.delete('/:contactId', deleteContact);
-=======
-router.use(authenticate); // Додаємо middleware для аутентифікації
-
-router.get('/', getAllContactsController);
-router.get('/:contactId', getContactByIdController);
-router.post('/', validateBody(contactSchema), createContactController);
-router.patch(
-  '/:contactId',
-  validateBody(contactSchema),
-  updateContactController,
-);
-router.delete('/:contactId', deleteContactController);
->>>>>>> Stashed changes
 
 export default router;
